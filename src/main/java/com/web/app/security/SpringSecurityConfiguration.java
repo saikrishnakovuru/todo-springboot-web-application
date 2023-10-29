46dec63b48e2e6e32d9ebf1cdc9cb0acd4bab348
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2023-10-29 15:54:24 -0400

    Comment on why the method is needed

--- src/main/java/com/web/app/security/SpringSecurityConfiguration.java
+++ src/main/java/com/web/app/security/SpringSecurityConfiguration.java
@@ -42,6 +42,7 @@ public class SpringSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    // Below method is to elemenate the 403 error on localhost:8080/h2-console
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
