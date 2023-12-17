d273a0b25053942b9f191cb472d43d679a59224b
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2023-12-17 10:50:38 -0500

    test

--- src/main/java/com/web/app/todo/controller/TodoController.java
+++ src/main/java/com/web/app/todo/controller/TodoController.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
